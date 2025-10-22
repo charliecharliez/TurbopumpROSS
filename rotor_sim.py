b9c8fda0908f08d1faf2769cfd5f9db23e5f6bc2
charliecharliez <charliezhufr@gmail.com>
2025-10-22 00:01:26 -0400

    Update rotor_sim.py

--- rotor_sim.py
+++ rotor_sim.py
@@ -30,52 +30,44 @@ steel = rs.Material(name="Steel", rho=7810, E=211e9, G_s=81.2e9)
 
 shaft_material = ss_304;
 
-# turbine start
-shaft1 = rs.ShaftElement(n=0,
-    L=0.086939,
-    idl=0,
-    odl=0.010099,
-    material=shaft_material,
-    gyroscopic=True
-    );
+L = [0.086939, 0.027889, 0.017071, 0.018489]
+idl = [0] * len(L)
+odl = [0.010099, 0.012, 0.009525, 0.006350]
 
-# laby section
-shaft2 = rs.ShaftElement(
+laby_conical_shaft = rs.ShaftElement(
     L=0.03860800,
     idl=0,
     odl=0.016002,
     idr=0,
     odr=0.018796,
     material=shaft_material,
-    gyroscopic=True
+    gyroscopic=True,
+    shear_effects=True,
+    rotary_inertia=True
     );
 
-# LOX bearing section
-shaft3 = rs.ShaftElement(
-    L=0.027889,
-    idl=0,
-    odl=0.012,
-    material=shaft_material,
-    gyroscopic=True
-)
+shaft_elements = [
+    rs.ShaftElement(
+        L = L[i],
+        idl=idl[i],
+        odl=odl[i],
+        material=shaft_material,
+        gyroscopic=True,
+        shear_effects=True,
+        rotary_inertia=True
+    )
+    for i in range(len(L))
+];
 
-# LOX impeller section
-shaft4 = rs.ShaftElement(
-    L=0.017071,
-    idl=0,
-    odl=0.009525,
-    material=shaft_material,
-    gyroscopic=True
-)
+shaft_elements.insert(1, laby_conical_shaft)
 
-# LOX retaining nut section
-shaft4 = rs.ShaftElement(
-    L=0.018489,
-    idl=0,
-    odl=0.006350,
-    material=shaft_material,
-    gyroscopic=True
-)
+rotor_shaft = rs.Rotor(shaft_elements=shaft_elements);
+
+insert_nodes = [
+
+]
+
+rotor_model = rotor_shaft.add_nodes(insert_nodes)
 
 # SI units btw
 turbine = rs.DiskElement(n=0, m=0.292578, Ip = 3.68819E-4, Id=1.85449E-4, tag="Turbine");
@@ -86,14 +78,10 @@ kero_bearing1 = rs.BallBearingElement(
     n=1, n_balls=12, d_balls=5.556E-3,
     fs=0, alpha=bearing_alpha)
 
-shaft_elements = [shaft1, shaft2, shaft3, shaft4];
 disk_elements = [turbine];
 bearing_elements = [kero_bearing1];
 
-rotor = rs.Rotor(
-    shaft_elements=shaft_elements,
-    disk_elements=disk_elements,
-    bearing_elements=bearing_elements
-)
+rotor_model.disk_elements = disk_elements
+rotor_model.bearing_elements = bearing_elements
 
-rotor.plot_rotor()
+rotor_model.plot_rotor()
